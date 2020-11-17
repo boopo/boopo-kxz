94cb64dcecada788d35b27af4837e7fd165ce621
Boopo <1662870160@qq.com>
2020-11-17 22:47:48 +0800

    增加了跨域请求设置

--- App/__init__.py
+++ App/__init__.py
@@ -1,14 +1,18 @@
 import redis
 from flask import Flask
+from flask_cors import CORS
+
+from App._settings import envs
 from App.apis import init_api
 from App.ext import init_ext, db
-from App._settings import envs
+
 from App.views import init_view
 
 
 def create_app(env):
     app = Flask(__name__)
-
+    # 跨域设置
+    CORS(app, supports_credentials=True)
     # 初始化配置
     app.config.from_object(envs.get(env))
     # 初始化API
--- App/ext.py
+++ App/ext.py
@@ -2,7 +2,6 @@ from flask_migrate import Migrate
 from flask_redis import FlaskRedis
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
 
 migrate = Migrate()
@@ -13,9 +12,9 @@ redis_third = FlaskRedis()
 
 redis_third.config_prefix = 'REDIS01'
 
+
 def init_ext(app):
     db.init_app(app)
     migrate.init_app(app, db)
     redis_client.init_app(app)
     redis_third.init_app(app)
-
